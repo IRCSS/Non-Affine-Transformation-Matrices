c5ffc71bb17951bd0aad32e83e45816cdd52b618
IRCSS <sh.shahrabi@hotmail.com>
2019-12-16 23:00:49 +0000

    added the simple non affine transformation along the y axis

---
+++ Assets/Scripts/SetOriginsAndAxis.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetOriginsAndAxis : MonoBehaviour {
+
+    private void Update()
+    {
+
+        Shader.SetGlobalMatrix("_worldToNeck", this.transform.worldToLocalMatrix);
+        Shader.SetGlobalMatrix("_NeckToWorld", this.transform.localToWorldMatrix);
+    }
+}
